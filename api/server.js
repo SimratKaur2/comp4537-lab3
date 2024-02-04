39cd624981d689bf94263a6dd8c24a261400e004
Simrat Kaur <simratkaur2@my.bcit.ca>
2024-02-04 01:39:06 -0800

    fixed up code in server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 // const http = require("http");
 // const path = require("path");
-const lang = require("../lang/en/en.json");
-const utils = require("../modules/utils");
+const lang = require("./lang/en/en.json");
+const utils = require("./modules/utils");
 
 // const PORT = process.env.PORT || 3000;
 
@@ -21,7 +21,7 @@ const utils = require("../modules/utils");
 // };
 
 module.exports = (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+  const url = new URL(`https://${req.headers.host}${req.url}`);
   const name = url.searchParams.get("name") || "Guest";
   const serverTime = utils.getDate();
   const message = lang.greeting.replace("%s", name).replace("%s", serverTime);
