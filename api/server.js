1e7aeacf19cd82a9b20611eac5eaab5db918a85d
Simrat Kaur <simratkaur2@my.bcit.ca>
2024-02-04 01:31:48 -0800

    changed up server.js for vercel deployment

--- api/server.js
+++ api/server.js
@@ -1,27 +1,37 @@
-const http = require("http");
-const path = require("path");
-const lang = require("./lang/en/en.json");
-const utils = require("./modules/utils");
+// const http = require("http");
+// const path = require("path");
+const lang = require("../lang/en/en.json");
+const utils = require("../modules/utils");
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-const requestHandler = (req, res) => {
-  if (req.url.startsWith("/COMP4537/labs/3/getDate")) {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const name = url.searchParams.get("name") || "Guest";
-    const serverTime = utils.getDate();
-    const message = lang.greeting.replace("%s", name).replace("%s", serverTime);
+// const requestHandler = (req, res) => {
+//   if (req.url.startsWith("/COMP4537/labs/3/getDate")) {
+//     const url = new URL(req.url, `http://${req.headers.host}`);
+//     const name = url.searchParams.get("name") || "Guest";
+//     const serverTime = utils.getDate();
+//     const message = lang.greeting.replace("%s", name).replace("%s", serverTime);
 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`${message}`);
-  } else {
-    res.writeHead(404);
-    res.end("Not Found");
-  }
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end(`${message}`);
+//   } else {
+//     res.writeHead(404);
+//     res.end("Not Found");
+//   }
+// };
+
+module.exports = (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const name = url.searchParams.get("name") || "Guest";
+  const serverTime = utils.getDate();
+  const message = lang.greeting.replace("%s", name).replace("%s", serverTime);
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`<div style="color: blue;">${message}</div>`);
 };
 
-const server = http.createServer(requestHandler);
+// const server = http.createServer(requestHandler);
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
